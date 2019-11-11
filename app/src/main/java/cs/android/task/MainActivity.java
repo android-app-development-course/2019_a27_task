6cd9eae16544602521fbdbbf3781fe86bb461505
Dav1dch <807557852@qq.com>
2019-11-11 14:51:33 +0800

    first commit

---
+++ app/src/main/java/cs/android/task/MainActivity.java
@@ -0,0 +1,13 @@
+package cs.android.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
