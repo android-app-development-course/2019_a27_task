44029917557a67efcc2113da20001fff056c13bf
Dav1dch <807557852@qq.com>
2019-11-12 15:56:54 +0800

    navigation bar

--- app/src/main/java/cs/android/task/MainActivity.java
+++ app/src/main/java/cs/android/task/MainActivity.java
@@ -1,7 +1,16 @@
 package cs.android.task;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView nav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                View ls = findViewById(R.id.layout_select);
+                switch (item.getItemId()) {
+                    case R.id.project:
+                    case R.id.more:
+                    case R.id.friend:
+                        ls.setVisibility(View.VISIBLE);
+                        return true;
+                    case R.id.my:
+                        ls.setVisibility(View.GONE);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
     }
+
 }
