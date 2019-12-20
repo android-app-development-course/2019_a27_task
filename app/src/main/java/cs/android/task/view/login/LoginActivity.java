0edf5bc5413852cd8e8d29562d6f29399fcfc235
sam5Lin <1150050235@qq.com>
2019-12-20 15:33:50 +0800

    sam new

--- app/src/main/java/cs/android/task/view/login/LoginActivity.java
+++ app/src/main/java/cs/android/task/view/login/LoginActivity.java
@@ -2,12 +2,9 @@ package cs.android.task.view.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.app.ActivityOptions;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,79 +13,58 @@ import android.widget.Toast;
 
 import cs.android.task.R;
 import cs.android.task.view.main.MainActivity;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import task.LoginGrpc;
-import task.LoginOuterClass;
+import cs.android.task.view.signup.SignupActivity;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-  private EditText username;
-  private EditText password;
-  private Button signin;
-  private TextView signup;
-  private String uname;
-  private String pwd;
+    private EditText phone;
+    private EditText password;
+    private Button signin;
+    private TextView signup;
+    private String phone_str;
+    private String pwd_str;
 
-  private static String host = "10.242.2.42";
-  private static int port = 50050;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        signin = findViewById(R.id.signin);
+        signin.setOnClickListener(this);
+        signup = findViewById(R.id.goToSignup);
+        signup.setOnClickListener(this);
+        phone = findViewById(R.id.signin_phone);
+        password = findViewById(R.id.signin_password);
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_login);
-    setWidget();
-    signin.setOnClickListener(this);
-  }
-
-  public void setWidget() {
-    username = findViewById(R.id.username);
-    password = findViewById(R.id.password);
-    signin = findViewById(R.id.signin);
-    signup = findViewById(R.id.signup);
-  }
-
-  @Override
-  public void onClick(View v) {
-    switch (v.getId()) {
-      case R.id.signin:
-        Intent profile = new Intent(this, MainActivity.class);
-        String token = Login(username.getText().toString(), password.getText().toString());
-
-        profile.putExtra("phone_num", username.getText());
-        profile.putExtra("token", token);
-        startActivity(profile,
-                ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
-        break;
-      default:
-        break;
     }
-  }
 
-  /**
-   * Login send request for login, and return a token
-   * A token and username(phone number) must be send to main activity.
-   * You should validate phone number and password before you send request.
-   *
-   * @param phoneNum the phone num
-   * @param passwd the passwd
-   * @return token - string
-   */
-  public String Login(String phoneNum, String passwd) {
-    ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port)
-            .usePlaintext().build();
-    LoginGrpc.LoginBlockingStub blockingStub = LoginGrpc.newBlockingStub(channel);
-    LoginOuterClass.LoginInfo loginInfo = LoginOuterClass.LoginInfo.newBuilder()
-            .setPhoneNum(phoneNum)
-            .setPassword(passwd)
-            .build();
-    LoginOuterClass.Token token = blockingStub.login(loginInfo);
-    Log.d("login", token.getToken());
-    if (!token.getToken().isEmpty()) {
-      Toast.makeText(getBaseContext(), "fail to login", Toast.LENGTH_SHORT).show();
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.signin:
+                phone_str = phone.getText().toString();
+                pwd_str = phone.getText().toString();
+                if(signUpSuccess()){
+                    Toast.makeText(this,"Sign in Success",Toast.LENGTH_LONG).show();
+                    Intent profile = new Intent(this, MainActivity.class);
+                    startActivity(profile);
+                }
+                else{
+                    Toast.makeText(this,"Sign in Fail",Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.goToSignup:
+                Intent profile1 = new Intent(this, SignupActivity.class);
+                startActivity(profile1);
+                break;
+            default:
+                break;
+        }
     }
-    channel.shutdown();
-    return token.getToken();
-  }
 
-  public void signIn() {}
+    public boolean signUpSuccess(){
+        if(!(" ".equals(phone_str)) && phone_str.length() != 0 && !(" ".equals(pwd_str)) && pwd_str.length() != 0){
+            //接口
+            return true;
+        }
+        return true;
+    }
 }
